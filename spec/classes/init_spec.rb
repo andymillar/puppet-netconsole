22618c7941a71c947be1613aface06024ece52c3
Andy Millar <andy@andymillar.co.uk>
2015-08-08 14:06:39 +0000

    TEST: Add in testing for netconsole::localport constraints

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -14,4 +14,64 @@ describe 'netconsole' do
     }
     it { should contain_class('netconsole') }
   end
+
+  context 'too low localport' do
+    let (:facts){
+      {
+        :puppetversion          => ENV['PUPPET_VERSION'],
+        :facterversion          => ENV['FACTER_VERSION'],
+        :osfamily               => 'redhat',
+        :operatingsystem        => 'centos',
+        :operatingsystemrelease => '6.6',
+        :concat_basedir         => '/dnf',
+      }
+    }
+
+    let(:params) { { :localport => 0 } }
+    it do
+      expect {
+        should compile
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /Expected 0 to be greater or equal to 1/)
+    end
+  end
+
+  context 'too high localport' do
+    let (:facts){
+      {
+        :puppetversion          => ENV['PUPPET_VERSION'],
+        :facterversion          => ENV['FACTER_VERSION'],
+        :osfamily               => 'redhat',
+        :operatingsystem        => 'centos',
+        :operatingsystemrelease => '6.6',
+        :concat_basedir         => '/dnf',
+      }
+    }
+
+    let(:params) { { :localport => 100000 } }
+    it do
+      expect {
+        should compile
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /Expected 100000 to be smaller or equal to 65535/)
+    end
+  end
+
+  context 'valid localport' do
+    let (:facts){
+      {
+        :puppetversion          => ENV['PUPPET_VERSION'],
+        :facterversion          => ENV['FACTER_VERSION'],
+        :osfamily               => 'redhat',
+        :operatingsystem        => 'centos',
+        :operatingsystemrelease => '6.6',
+        :concat_basedir         => '/dnf',
+      }
+    }
+
+    let(:params) { { :localport => 512 } }
+
+    it { should contain_class('netconsole') }
+  end
+
+
+
 end
